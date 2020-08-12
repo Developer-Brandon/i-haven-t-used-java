e3aa9d908f3560acd527ccaa9ccaa4479afddf4b
leedokuem <lkc2252@gmail.com>
2020-08-12 19:56:07 +0900

    UnicodeChar was fisnihed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        char ch1='A';
+        char ch2='한';
+        char ch3=0x3091;
+        char ch4=0x3092;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+        System.out.println(ch4);
     }
 }
