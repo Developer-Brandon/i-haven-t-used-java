2d2fe059558556bee4ed098f1b96c10ebeeacca3
leedokuem <lkc2252@gmail.com>
2020-08-12 18:24:54 +0900

    - First example was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Java");
+        System.out.println("First Java program");
+        // 1. Java source code를 compiler가 bite code로 변경
+        // 2. Example.java -> 이게 java scource code
+        // 3. Java byte code란? -> java compiler에 의해 생성되는 코드이다.
+        // 결론 Example.java -> [javac.exe(javac compiler)] compile.... -> [Example.class | Java virtual machine] 이 두개는 java.exe에 의해 실행됨
     }
 }
