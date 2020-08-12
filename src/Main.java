6767ee0f296e94c042314af39efbad2949b21c69
leedokuem <lkc2252@gmail.com>
2020-08-12 22:21:53 +0900

    88p-4-1-4 was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int A = 3+6;
+        int B = A+9;
+        int C = B+12;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
     }
 }
