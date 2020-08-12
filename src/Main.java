4410902be198b5274ab696befcf1fe74e36b46e1
leedokuem <lkc2252@gmail.com>
2020-08-12 21:59:24 +0900

    88p-4-1-1 was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1,num2,num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = 30;
+
+        num1 = num2 = num3;
+
+        System.out.println("Result of num1 - " + num1);
     }
 }
