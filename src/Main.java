b249e26b494611f3c604970c5cef1f851215bc0c
leedokuem <lkc2252@gmail.com>
2020-08-12 18:46:21 +0900

    VaraibleDecl was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double num1, num2, result;
+        num1=1.00001;
+        num2=2.00002;
+        result=num1+num2;
+        System.out.println(result);
     }
 }
