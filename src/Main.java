1f08590b5f5cf4839f6d03b22de17da5b574fce2
leedokuem <lkc2252@gmail.com>
2020-08-12 21:50:18 +0900

    SCE was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1=0, num2=0;
+        boolean result, result2;
+
+        result = (num1+=10)<0 && (num2+=10)>0;
+        System.out.println("result="+result);
+        System.out.println("num1="+num1+", num2="+num2);
+
+        result2 = (num1+=10)>0 || (num2+=10)>0;
+        System.out.println("result2="+result2);
+        System.out.println("num1="+num1+", num2="+num2);
     }
 }
