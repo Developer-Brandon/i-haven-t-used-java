b5389f9dfe950f57f9e43b5d04a3d3c121a72920
leedokuem <lkc2252@gmail.com>
2020-08-18 15:07:07 +0900

    88p-4-1-5 was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int A = ((25+5)+(36/4)-72)*5;
+        int B = ((25*5)+(36-4)+71)/4;
+        int C = (128/4)*2;
+
+        boolean isBig = (A>B) && (B>C);
+        System.out.println(isBig);
     }
 }
