fe2e23933aee45e50a43e197e358b384d955fa19
leedokuem <lkc2252@gmail.com>
2020-08-18 17:25:58 +0900

    131p-5-5 was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // example 1
+        int sum = 1;
+        for (int i = 1,j = 10; i<=j; i++) {
+            sum = sum * i;
+            System.out.println("1부터 10까지 곱하는 과정: " + sum);
+        }
+        System.out.println("1부터 10까지 곱하는 결과: " + sum);
+
+        // example 2
+        int standardValueOfGuGudan = 5;
+        for (int i = 1, j = 9; i<=j; i++) {
+            System.out.println("구구단을 외자(5단): " + standardValueOfGuGudan * i);
+        }
     }
 }
