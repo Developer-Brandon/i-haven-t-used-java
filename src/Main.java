65f701218dcbd3beb6a4408ce4c407a4112bb163
leedokuem <lkc2252@gmail.com>
2020-08-18 18:01:52 +0900

    143p-5-7 was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i <= 8; i += 2) {
+            if (i > 0) {
+                if (i == 2 || i == 4 || i == 6 || i == 8) {
+                    for (int j = 1; j <= i; j++) {
+                        System.out.println("구구단 " + i + "단: " + i + " x " + j + " = " + i * j);
+                    }
+                }
+            }
+        }
+
     }
 }
