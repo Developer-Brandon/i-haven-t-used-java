06fa42ae22211fbb7e64f1dbba4a2943972617bd
leedokuem <lkc2252@gmail.com>
2020-08-18 15:55:23 +0900

    feature/115p-1-2 was finished

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        int num = 120;
-        if (num > 0 && (num % 2) == 0)
-            System.out.println("num은 양수이면서 짝수입니다");
+        int num = 220;
+        if(num < 0) {
+            System.out.println("0미만");
+        } else if(num < 100) {
+            System.out.println("0이상 100미만");
+        } else if(num < 200) {
+            System.out.println("100이상 200미만");
+        } else if(num < 300) {
+            System.out.println("200이상 300미만");
+        } else if(num >= 300) {
+            System.out.println("300이상");
+        }
     }
 }
