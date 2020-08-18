1a8419242d1d13872d3ca89a9c76646e50925779
leedokuem <lkc2252@gmail.com>
2020-08-18 16:07:21 +0900

    SwitchBasic was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int n = 3;
+        switch (n) {
+            case 1:
+                System.out.println("Simple Java");
+            case 2:
+                System.out.println("Funny Java");
+            case 3:
+                System.out.println("Fantastic Java");
+            default:
+                System.out.println("The best programming language");
+        }
+        System.out.println("Do you like coffee?");
     }
 }
