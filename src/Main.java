fec04c1bdbe3e73393a3345f0591aee9e6466ac0
leedokuem <lkc2252@gmail.com>
2020-08-18 15:42:39 +0900

    Unaryadmmin was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int n1 = 5;
+        System.out.println(+n1);
+        System.out.println(-n1);
+
+        short n2 = 7;
+        int n3 = +n2;
+        int n4 = -n2;
+        System.out.println(n3);
+        System.out.println(n4);
     }
 }
