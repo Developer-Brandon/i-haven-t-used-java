15f0c5f0aac4c7782bb26620cbc62b34f3d0f054
leedokuem <lkc2252@gmail.com>
2020-08-18 20:00:19 +0900

    InfRecul was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
+    public static void showHi(int cnt) {
+        System.out.println("Hi~");
+        showHi(cnt--);
+        if (cnt == 1)
+            return;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        showHi(3);
     }
 }
