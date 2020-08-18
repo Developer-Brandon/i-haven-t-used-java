dbcae05866ad538f02161fca0a4fff6f6add58e3
leedokuem <lkc2252@gmail.com>
2020-08-19 00:20:36 +0900

    passInstance was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+class Number {
+    int num = 0;
+    public void addNum(int n) {
+        num += n;
+    }
+    public int getNumber() {
+        return num;
+    }
+}
+
 public class Main {
+    public static void simpleMethod(Number numberParam) {
+        numberParam.addNum(12);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Number newInstance = new Number();
+        System.out.println("메소드 호출 전: " + newInstance.getNumber());
+
+        simpleMethod(newInstance);
+        System.out.println("메소드 호출 후: " + newInstance.getNumber());
     }
 }
