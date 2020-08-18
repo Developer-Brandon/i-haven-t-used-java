ffa8d3acc404bc5d975b8d2d3bbbd5a72b6c9bdb
leedokuem <lkc2252@gmail.com>
2020-08-19 01:29:12 +0900

    fruitSalesMain was finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
-public class Main {
+class FruitSeller {
+    int numOfApple;
+    int myMoney;
+    int APPLE_PRICE;
 
+    public int saleApple(int money) {
+        int num = money / APPLE_PRICE;
+        numOfApple -= num;
+        myMoney += money;
+        return num;
+    }
+
+    public void showSaleResult () {
+        System.out.println("남은 사과 : " + numOfApple);
+        System.out.println("판매 수익 : " + myMoney);
+        System.out.println("");
+    }
+
+    public void initMembers(int money, int appleNum, int price) {
+        myMoney = money;
+        numOfApple = appleNum;
+        APPLE_PRICE = price;
+    }
+}
+
+class FruitBuyer {
+    int myMoney = 10000;
+    int numOfApple = 0;
+
+    public void buyApple(FruitSeller seller,  int money) {
+        numOfApple += seller.saleApple(money);
+        myMoney -= money;
+    }
+    public void showBuyResult() {
+        System.out.println("현재 잔액 : " + myMoney);
+        System.out.println("사과 개수 : " + numOfApple);
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FruitSeller seller1 = new FruitSeller();
+        seller1.initMembers(0, 30, 1500);
+
+        FruitSeller seller2 = new FruitSeller();
+        seller2.initMembers(0,20 , 1000);
+
+        FruitBuyer buyer = new FruitBuyer();
+        buyer.buyApple(seller1, 4500);
+        buyer.buyApple(seller2,2000 );
+
+        System.out.println("과일 판매자 1의 현재 상황");
+        seller1.showSaleResult();
+
+        System.out.println("과일 판매자 2의 현재 상황");
+        seller2.showSaleResult();
+
+        System.out.println("과일 구매자의 현재 상황");
+        buyer.showBuyResult();
+
     }
 }
